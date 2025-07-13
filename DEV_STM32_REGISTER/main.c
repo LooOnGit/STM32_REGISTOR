d474144234cdc0cb3b84ed64d6793697f140c164
LooOnGit <Email  ng k GitHub>
2025-07-13 09:28:07 +0700

    Add config FreeRTOS

--- DEV_STM32_REGISTER/main.c
+++ DEV_STM32_REGISTER/main.c
@@ -3,7 +3,30 @@
 #include "delay.h"
 #include "clock.h"
 #include "pwm.h"
-#include "low_power.h"
+// #include "low_power.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+void vApplicationMallocFailedHook()
+{
+
+}
+
+void vApplicationTickHook()
+{
+
+}
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask,
+                                        char * pcTaskName )
+{
+	
+}
+
+void vApplicationIdleHook()
+{
+
+}
 
 void SystemInit ();
 void setup()
@@ -14,23 +37,50 @@ void setup()
     // pwm_init();
 }
 
+void func_1(void* param)
+{
+	while(1)
+	{
+		led_control(BLUE, ON);
+		vTaskDelay(1000);
+		led_control(BLUE, OFF);
+		vTaskDelay(1000);
+	}
+}
+
+void func_2(void* param)
+{
+	while(1)
+	{
+		led_control(RED, ON);
+		vTaskDelay(1500);
+		led_control(RED, OFF);
+		vTaskDelay(1000);
+	}
+}
+
 int main(){
     setup();
     int cnt = 0;
-    enable_iwdg();
+    // enable_iwdg();
+    TaskHandle_t task_1 = NULL;
+	TaskHandle_t task_2 = NULL;
+    xTaskCreate(func_1, "task 1", 512, NULL, 0, &task_1);
+	xTaskCreate(func_2, "task 2", 512, NULL, 0, &task_2);
 
+    vTaskStartScheduler();
     while(1){
         // led_control(ORANGE, ON);
-        // // delay(1000);
-        // // led_control(ORANGE, OFF);
-        // // delay(1000);    
+        // delay(1000);
+        // led_control(ORANGE, OFF);
+        // delay(1000);    
         // if(++cnt > 100)
         //     cnt = 0;
         // pwm_pulse_crtl(cnt);
         // delay(10);
 
-        delay(5000);
-        goto_low_power_mode();
+        // delay(5000);
+        // goto_low_power_mode();
     }
     return 0;
 }
